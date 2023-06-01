0b679ebbee4ea1a6d9c3fd2735678dd098522001
Alsaze <116272173+Alsaze@users.noreply.github.com>
2023-06-01 16:46:34 +0300

    Update
    +Teleport
    Fix bags with player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,22 +5,34 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private GameObject open; 
+    [SerializeField] private GameObject open;
+
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject teleport;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Win")
         {
-            Debug.Log("You win!!");            
-        }
-        if (collision.gameObject.tag == "Lose")
-        {
-            Debug.Log("You Lose!!");            
+            Debug.Log("You win!!");
         }
 
-        if (collision.gameObject.tag=="Open")
+        if (collision.gameObject.tag == "Lose")
+        {
+            Debug.Log("You Lose!!");
+        }
+
+        if (collision.gameObject.tag == "Open")
         {
             Debug.Log("Open");
             Destroy(open);
+        }
+
+        if (collision.gameObject.tag == "Teleport")
+        {
+            Debug.Log("Teleport");
+            Vector3 vector3Teleport = teleport.transform.position;
+            player.transform.position = vector3Teleport;
         }
     }
 }
